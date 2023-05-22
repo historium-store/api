d6074bbfedd866fcd8071989161507dde28111be
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-05-22 14:33:18 +0300

    get user account hotfix

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import createHttpError from 'http-errors';
 import { authenticate } from '../controllers/authController.js';
 import userController from '../controllers/userController.js';
 import { checkRole } from '../middleware/index.js';
@@ -9,11 +10,42 @@ import {
 
 const router = Router();
 
+router.get('/account', authenticate, (req, res) => {
+	res.json({ status: 'OK', data: req.user });
+});
+
 router
 	.route('/:id')
-	.get(validateId, userController.getOne)
-	.patch(validateUpdate, userController.updateOne)
-	.delete(validateId, userController.deleteOne);
+	.get(
+		authenticate,
+		checkRole(['admin']),
+		validateId,
+		userController.getOne
+	)
+	.patch(
+		authenticate,
+		validateUpdate,
+		(req, res, next) => {
+			if (req.user.id === req.params.id) {
+				return next();
+			}
+
+			next(createHttpError(403));
+		},
+		userController.updateOne
+	)
+	.delete(
+		authenticate,
+		validateId,
+		(req, res, next) => {
+			if (req.user.id === req.params.id) {
+				return next();
+			}
+
+			next(createHttpError(403));
+		},
+		userController.deleteOne
+	);
 
 router.get(
 	'/',
