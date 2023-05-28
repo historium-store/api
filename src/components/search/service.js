9c90e8a3188bf947a4a6ba796ef89f30218751de
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-05-29 01:03:13 +0300

    change search for products to search for books

--- src/components/search/service.js
+++ src/components/search/service.js
@@ -1,12 +1,30 @@
+import Book from '../book/model.js';
 import Product from '../product/model.js';
 
 const findProducts = async valueToFind => {
-	return await Product.find().where({
-		$or: [
-			{ name: { $regex: valueToFind, $options: 'i' } },
-			{ code: valueToFind }
-		]
-	});
+	const productIds = (
+		await Product.find().where({
+			$or: [
+				{ name: { $regex: valueToFind, $options: 'i' } },
+				{ code: valueToFind }
+			]
+		})
+	).map(p => p.id);
+
+	return await Book.find({ product: productIds })
+		.populate([
+			'publisher',
+			'series',
+			'authors',
+			'compilers',
+			'translators',
+			'illustrators',
+			'editors'
+		])
+		.populate({
+			path: 'product',
+			populate: ['type', 'sections']
+		});
 };
 
 export default { findProducts };
