bf604ae3bd8436904719d0aedbf4a006229161e5
DobriyIm <dobriy.edu@gmail.com>
2023-06-09 22:19:04 +0300

    fix 'server.js' file

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ import app from './src/app.js';
 const PORT = process.env.PORT || 3000;
 
 mongoose
-	.connect(process.env.CONNECTION_STRING, {
-		dbName: 'historium-db'
-	})
+	.connect(process.env.CONNECTION_STRING)
 	.then(() =>
 		app.listen(PORT, () =>
 			console.log(`API is listening on port ${PORT}`)
