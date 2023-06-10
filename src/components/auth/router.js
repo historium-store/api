9fec3a69afed6ecff62054b5b2c834210bb4d49b
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-06-11 01:29:19 +0300

    update user auth documentation

--- src/components/auth/router.js
+++ src/components/auth/router.js
@@ -10,40 +10,91 @@ const authRouter = Router();
  *   post:
  *     tags:
  *       - user
- *     summary: User signup
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             $ref: '#/components/schemas/SignupRequest'
- *           example:
- *             firstName: John
- *             lastName: Williams
- *             phoneNumber: '+380442138972'
- *             email: john.williams@gmail.com
- *             password: '12345678'
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               phoneNumber:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *             required:
+ *               - firstName
+ *               - lastName
+ *               - phoneNumber
+ *               - email
+ *               - password
+ *             example:
+ *               firstName: John
+ *               lastName: Williams
+ *               phoneNumber: '+380442138972'
+ *               email: john.williams@gmail.com
+ *               password: '12345678'
  *     responses:
  *       '201':
  *         description: User successfully signed up
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/SignupResponse'
- *             example:
- *               _id: 617268ce1a9b261b6c35cc1d
- *               firstName: John
- *               lastName: Williams
- *               phoneNumber: '+380442138972'
- *               email: john.williams@gmail.com
- *               password: d74eef53e89912dc618537ae00e91347d78747e1dd7f6fcfa26732f23fcfa79c
- *               salt: c42af8db45f10eefb83d52cfd58a0b6e
- *               role: user
- *               reviews: []
- *               favorites: []
- *               createdAt: 1686387456078
- *               updatedAt: 1686387456078
- *               cart: 617c9e5d4c5ad0c2a95e9b1f
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                 firstName:
+ *                   type: string
+ *                 lastName:
+ *                   type: string
+ *                 phoneNumber:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *                   format: email
+ *                 password:
+ *                   type: string
+ *                 salt:
+ *                   type: string
+ *                 role:
+ *                   type: string
+ *                 reviews:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                 favorites:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                 createdAt:
+ *                   type: number
+ *                   format: int64
+ *                 updatedAt:
+ *                   type: number
+ *                   format: int64
+ *                 cart:
+ *                   type: string
+ *               example:
+ *                 _id: 617268ce1a9b261b6c35cc1d
+ *                 firstName: John
+ *                 lastName: Williams
+ *                 phoneNumber: '+380442138972'
+ *                 email: john.williams@gmail.com
+ *                 password: d74eef53e89912dc618537ae00e91347d78747e1dd7f6fcfa26732f23fcfa79c
+ *                 salt: c42af8db45f10eefb83d52cfd58a0b6e
+ *                 role: user
+ *                 reviews: []
+ *                 favorites: []
+ *                 createdAt: 1686387456078
+ *                 updatedAt: 1686387456078
+ *                 cart: 617c9e5d4c5ad0c2a95e9b1f
  *       '409':
  *         description: User already exists
  *         content:
@@ -70,24 +121,33 @@ authRouter.post(
  * @swagger
  * /login:
  *   post:
- *     summary: User login
+ *     tags:
+ *       - user
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             $ref: '#/components/schemas/LoginRequest'
+ *             type: object
+ *             properties:
+ *               login:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - login
+ *               - password
  *           examples:
- *                 phoneNumber:
- *                   summary: Using phone number
- *                   value:
- *                     login: '+380442138972'
- *                     password: '12345678'
- *                 emailExists:
- *                   summary: Using email
- *                   value:
- *                     login: john.williams@gmail.com
- *                     password: '12345678'
+ *             phoneNumber:
+ *               summary: Using phone number
+ *               value:
+ *                 login: '+380442138972'
+ *                 password: '12345678'
+ *             emailExists:
+ *               summary: Using email
+ *               value:
+ *                 login: john.williams@gmail.com
+ *                 password: '12345678'
  *     responses:
  *       '200':
  *         description: User successfully logged in
@@ -120,75 +180,6 @@ export default authRouter;
  * @swagger
  * components:
  *   schemas:
- *
- *     SignupRequest:
- *       type: object
- *       properties:
- *         firstName:
- *           type: string
- *         lastName:
- *           type: string
- *         phoneNumber:
- *           type: string
- *         email:
- *           type: string
- *           format: email
- *         password:
- *           type: string
- *       required:
- *         - firstName
- *         - lastName
- *         - phoneNumber
- *         - email
- *         - password
- *
- *     SignupResponse:
- *       type: object
- *       properties:
- *         _id:
- *           type: string
- *         firstName:
- *           type: string
- *         lastName:
- *           type: string
- *         phoneNumber:
- *           type: string
- *         email:
- *           type: string
- *           format: email
- *         password:
- *           type: string
- *         salt:
- *           type: string
- *         role:
- *           type: string
- *         reviews:
- *           type: array
- *           items:
- *             type: string
- *         favorites:
- *           type: array
- *           items:
- *             type: string
- *         createdAt:
- *           type: number
- *           format: int64
- *         updatedAt:
- *           type: number
- *           format: int64
- *         cart:
- *           type: string
- *
- *     LoginRequest:
- *       type: object
- *       properties:
- *         login:
- *           type: string
- *         password:
- *           type: string
- *       required:
- *         - login
- *         - password
  *
  *     Error:
  *       type: object
