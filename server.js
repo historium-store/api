2877188512a69af4411d8c6cd9402e1f1a5ca215
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-06-16 00:18:29 +0300

    comment importData

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 import mongoose from 'mongoose';
 import importData from './db-init.js';
 import app from './src/app.js';
-import Section from './src/components/section/model.js';
 
-importData().then(() => {
-	const PORT = process.env.PORT || 3000;
+// importData().then(() => {
+const PORT = process.env.PORT || 3000;
 
-	mongoose
-		.connect(process.env.CONNECTION_STRING)
-		.then(() =>
-			app.listen(PORT, () =>
-				console.log(`API is listening on port ${PORT}`)
-			)
+mongoose
+	.connect(process.env.CONNECTION_STRING)
+	.then(() =>
+		app.listen(PORT, () =>
+			console.log(`API is listening on port ${PORT}`)
 		)
-		.catch(err => {
-			console.log(`Failed to connect to database: ${err.message}`);
-		});
-
-	process.on('unhandledRejection', event => {
-		console.log(event.promise);
-		console.log(event.reason);
+	)
+	.catch(err => {
+		console.log(`Failed to connect to database: ${err.message}`);
 	});
+
+process.on('unhandledRejection', event => {
+	console.log(event.promise);
+	console.log(event.reason);
 });
+// });
