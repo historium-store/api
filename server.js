9ed702f20a15a34cf540e5dc243280507040c30a
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-06-16 13:56:56 +0300

    uncomment backup code

--- server.js
+++ server.js
@@ -2,22 +2,24 @@ import mongoose from 'mongoose';
 import importData from './db-init.js';
 import app from './src/app.js';
 
-// importData().then(() => {
-const PORT = process.env.PORT || 3000;
+importData().then(() => {
+	const PORT = process.env.PORT || 3000;
 
-mongoose
-	.connect(process.env.CONNECTION_STRING)
-	.then(() =>
-		app.listen(PORT, () =>
-			console.log(`API is listening on port ${PORT}`)
+	mongoose
+		.connect(process.env.CONNECTION_STRING, {
+			dbName: 'historium-db'
+		})
+		.then(() =>
+			app.listen(PORT, () =>
+				console.log(`API is listening on port ${PORT}`)
+			)
 		)
-	)
-	.catch(err => {
-		console.log(`Failed to connect to database: ${err.message}`);
-	});
+		.catch(err => {
+			console.log(`Failed to connect to database: ${err.message}`);
+		});
 
-process.on('unhandledRejection', event => {
-	console.log(event.promise);
-	console.log(event.reason);
+	process.on('unhandledRejection', event => {
+		console.log(event.promise);
+		console.log(event.reason);
+	});
 });
-// });
