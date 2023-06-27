179e19bfb4c65aa57b51c999c8f0d73f9fbf3c80
DobriyIm <dobriy.edu@gmail.com>
2023-06-27 17:03:20 +0300

    update 'product-code' trigger

--- src/triggers/product-code.js
+++ src/triggers/product-code.js
@@ -3,30 +3,15 @@ import mongoose from 'mongoose';
 export const setProductCode = async doc => {
 	try {
 		await mongoose.connection
-			.collection('productCodeCounter')
+			.collection('product_code_counter')
 			.updateOne({}, { $inc: { currentCode: 1 } });
 
 		const productCodeCounter = await mongoose.connection
-			.collection('productCodeCounter')
+			.collection('product_code_counter')
 			.findOne();
 
 		await doc.set('code', productCodeCounter.currentCode);
 	} catch (err) {
-		throw {
-			status: err.status ?? 500,
-			message: err.message ?? err
-		};
-	}
-};
-
-export const getProductCode = async () => {
-	try {
-		const productCodeCounter = await mongoose.connection
-			.collection('productCodeCounter')
-			.findOne();
-
-		return productCodeCounter.currentCode;
-	} catch (error) {
 		throw {
 			status: err.status ?? 500,
 			message: err.message ?? err
