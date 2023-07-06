94c4d862fed29372caf988b76e8eb2fc5179a298
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-07-06 16:49:08 +0300

    update backup

--- src/components/book/model.js
+++ src/components/book/model.js
@@ -38,8 +38,8 @@ const bookSchema = new Schema(
 			type: [
 				{
 					type: ObjectId,
-					ref: 'Author',
-					required: false
+					ref: 'Author'
+					// required: false
 				}
 			],
 			requried: false
@@ -49,8 +49,8 @@ const bookSchema = new Schema(
 			type: [
 				{
 					type: ObjectId,
-					ref: 'Compiler',
-					required: false
+					ref: 'Compiler'
+					// required: false
 				}
 			],
 			requried: false
@@ -60,144 +60,144 @@ const bookSchema = new Schema(
 			type: [
 				{
 					type: ObjectId,
-					ref: 'Translator',
-					required: false
+					ref: 'Translator'
+					// required: false
 				}
-			],
-			required: false
+			]
+			// required: false
 		},
 
 		illustrators: {
 			type: [
 				{
 					type: ObjectId,
-					ref: 'Illustrator',
-					required: false
+					ref: 'Illustrator'
+					// required: false
 				}
-			],
-			required: false
+			]
+			// required: false
 		},
 
 		editors: {
 			type: [
 				{
 					type: ObjectId,
-					ref: 'Editor',
-					required: false
+					ref: 'Editor'
+					// required: false
 				}
-			],
-			required: false
+			]
+			// required: false
 		},
 
 		series: {
 			type: ObjectId,
-			ref: 'BookSeries',
-			required: false
+			ref: 'BookSeries'
+			// required: false
 		},
 
 		copies: {
-			type: Number,
-			required: false
+			type: Number
+			// required: false
 		},
 
 		isbns: {
 			type: [
 				{
-					type: String,
-					required: false
+					type: String
+					// required: false
 				}
-			],
-			required: false
+			]
+			// required: false
 		},
 
 		firstPublishedIn: {
-			type: Number,
-			required: false
+			type: Number
+			// required: false
 		},
 
 		originalName: {
-			type: String,
-			required: false
+			type: String
+			// required: false
 		},
 
 		font: {
-			type: String,
-			required: false
+			type: String
+			// required: false
 		},
 
 		format: {
-			type: String,
-			required: false
+			type: String
+			// required: false
 		},
 
 		pages: {
-			type: Number,
-			required: false
+			type: Number
+			// required: false
 		},
 
 		weight: {
-			type: Number,
-			required: false
+			type: Number
+			// required: false
 		},
 
 		paperType: {
-			type: String,
-			required: false
+			type: String
+			// required: false
 		},
 
 		bindingType: {
-			type: String,
-			required: false
+			type: String
+			// required: false
 		},
 
 		illustrationsType: {
 			type: [
 				{
-					type: String,
-					required: false
+					type: String
+					// required: false
 				}
-			],
-			required: false
+			]
+			// required: false
 		},
 
 		literaturePeriod: {
 			type: [
 				{
-					type: String,
-					required: false
+					type: String
+					// required: false
 				}
-			],
-			required: false
+			]
+			// required: false
 		},
 
 		literatureCountry: {
 			type: [
 				{
-					type: String,
-					required: false
+					type: String
+					// required: false
 				}
-			],
-			required: false
+			]
+			// required: false
 		},
 
 		foreignLiterature: {
-			type: Boolean,
-			required: false
+			type: Boolean
+			// required: false
 		},
 
 		timePeriod: {
 			type: [
 				{
-					type: String,
-					required: false
+					type: String
+					// required: false
 				}
-			],
-			required: false
+			]
+			// required: false
 		},
 
 		grade: {
-			type: String,
-			required: false
+			type: String
+			// required: false
 		},
 
 		suitableAge: {
@@ -206,23 +206,23 @@ const bookSchema = new Schema(
 					type: String,
 					reqired: false
 				}
-			],
-			required: false
+			]
+			// required: false
 		},
 
 		packaging: {
-			type: String,
-			required: false
+			type: String
+			// required: false
 		},
 
 		occasion: {
 			tupe: [
 				{
-					type: String,
-					required: false
+					type: String
+					// required: false
 				}
-			],
-			required: false
+			]
+			// required: false
 		},
 
 		style: {
@@ -231,28 +231,28 @@ const bookSchema = new Schema(
 					type: String,
 					reqired: false
 				}
-			],
-			required: false
+			]
+			// required: false
 		},
 
 		excerpts: {
 			type: [
 				{
-					type: String,
-					required: false
+					type: String
+					// required: false
 				}
-			],
-			required: false
+			]
+			// required: false
 		},
 
 		suitableFor: {
 			type: [
 				{
-					type: String,
-					required: false
+					type: String
+					// required: false
 				}
-			],
-			required: false
+			]
+			// required: false
 		},
 
 		createdAt: {
@@ -264,8 +264,8 @@ const bookSchema = new Schema(
 		},
 
 		deletedAt: {
-			type: Number,
-			required: false
+			type: Number
+			// required: false
 		}
 	},
 	{
