0e88c2dcbd95160bec1689c467259a89986a929d
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-07-07 15:09:11 +0300

    update backup

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 import mongoose from 'mongoose';
 import importData from './db-init.js';
 import app from './src/app.js';
-import Product from './src/components/product/model.js';
-import User from './src/components/user/model.js';
-// import Product from './src/components/product/model.js';
 
 importData().then(() => {
 	const PORT = process.env.PORT || 3000;
@@ -11,24 +8,6 @@ importData().then(() => {
 		.connect(process.env.CONNECTION_STRING, {
 			dbName: 'historium-db'
 		})
-		// .then(async () => {
-		// 	const products = await Product.find().populate({
-		// 		path: 'specificProduct',
-		// 		model: 'Book',
-		// 		populate: 'authors'
-		// 	});
-
-		// 	for (let product of products) {
-		// 		product.creators = product.specificProduct.authors.map(
-		// 			a => a.fullName
-		// 		);
-
-		// 		product.set('specificProduct');
-		// 		product.set('model');
-
-		// 		await product.save();
-		// 	}
-		// })
 		.then(() =>
 			app.listen(PORT, () =>
 				console.log(`API is listening on port ${PORT}`)
