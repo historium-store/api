42682ce1830f453355a1c9294c195ae65cd2fc29
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-07-12 14:57:32 +0300

    add GET /board-game route

--- src/components/board-game/controller.js
+++ src/components/board-game/controller.js
@@ -26,7 +26,22 @@ const getOne = async (req, res, next) => {
 	}
 };
 
+const getAll = async (req, res, next) => {
+	try {
+		validationResult(req)
+			.formatWith(e => e.msg)
+			.throw();
+
+		const queryParams = matchedData(req);
+
+		res.json(await service.getAll(queryParams));
+	} catch (err) {
+		next(createError(err));
+	}
+};
+
 export default {
 	createOne,
-	getOne
+	getOne,
+	getAll
 };
