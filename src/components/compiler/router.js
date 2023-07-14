604e60c07a35444ddd04fbb1074a03331af73559
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-07-14 18:47:14 +0300

    update translator, compiler, illustrator & editor docs

--- src/components/compiler/router.js
+++ src/components/compiler/router.js
@@ -50,9 +50,24 @@ export default compilerRouter;
  *       - api_auth: []
  *     tags:
  *       - compiler
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               fullName:
+ *                 type: string
+ *             example:
+ *               fullName: Роман Клочко
  *     responses:
  *       '201':
  *         description: Created compiler
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CompilerResponse'
  *       '403':
  *         $ref: '#/components/responses/Forbidden'
  *   get:
@@ -67,6 +82,12 @@ export default compilerRouter;
  *     responses:
  *       '200':
  *         description: All compilers
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/CompilerResponse'
  * /compiler/{id}:
  *   get:
  *     summary: Get one compiler
@@ -77,6 +98,10 @@ export default compilerRouter;
  *     responses:
  *       '200':
  *         description: Requested compiler
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CompilerResponse'
  *       '403':
  *         $ref: '#/components/responses/Forbidden'
  *       '404':
@@ -89,9 +114,29 @@ export default compilerRouter;
  *       - compiler
  *     parameters:
  *       - $ref: '#/components/parameters/id'
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               fullName:
+ *                 type: string
+ *               books:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *             example:
+ *               books:
+ *                 - 6473b733569debe2438c78c0
+ *                 - 6473c260569debe2438c7906
  *     responses:
  *       '200':
- *         description: Requested compiler
+ *         description: Updated compiler
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CompilerResponse'
  *       '403':
  *         $ref: '#/components/responses/Forbidden'
  *       '404':
@@ -111,4 +156,29 @@ export default compilerRouter;
  *         $ref: '#/components/responses/Forbidden'
  *       '404':
  *         description: Compiler not found
+ * components:
+ *   schemas:
+ *     CompilerResponse:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *         fullName:
+ *           type: string
+ *         books:
+ *           type: array
+ *           items:
+ *             type: string
+ *         createdAt:
+ *           type: integer
+ *         updatedAt:
+ *           type: integer
+ *       example:
+ *         _id: 6473ab64c9dd40d6f991c5bc
+ *         fullName: Вікторія Стах
+ *         books:
+ *           - 6473b317569debe2438c7856
+ *           - 64b05745d2e35f26029e067d
+ *         createdAt: 1685302116345
+ *         updatedAt: 1689278277035
  */
